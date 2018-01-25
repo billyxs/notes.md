54cd20700a0eb2e1c72d094f361ad73bc2f04f64
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-25 09:47:03 -0800

    vim notes

---
+++ notes-vim/index.js
@@ -0,0 +1,7 @@
+# Vim
+
+## Articles
+
+[Vim Text Objects: The Definitive Guide](https://blog.carbonfive.com/2011/10/17/vim-text-objects-the-definitive-guide/)
+[https://www.quora.com/What-are-some-good-resources-for-learning-Vim]
+[Vim graphical cheat sheet](http://www.viemu.com/vi-vim-cheat-sheet.gif)
