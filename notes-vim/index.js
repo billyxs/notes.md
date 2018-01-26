38f0ec56fe38ac22c0d8edae9589713aaae51f1c
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-25 23:46:30 -0800

    rebuild: add vim talks

--- notes-vim/index.js
+++ notes-vim/index.js
@@ -5,3 +5,12 @@
 [Vim Text Objects: The Definitive Guide](https://blog.carbonfive.com/2011/10/17/vim-text-objects-the-definitive-guide/)
 [https://www.quora.com/What-are-some-good-resources-for-learning-Vim]
 [Vim graphical cheat sheet](http://www.viemu.com/vi-vim-cheat-sheet.gif)
+
+## Talks
+
++ [Learning Vim in a week](https://www.youtube.com/watch?v=_NUO4JEtkDw)
++ [Write code faster: expert-level vim](http://youtu.be/SkdrYWhh-8s)
++ [Impressive Ruby Productivity with Vim and Tmux](http://youtu.be/9jzWDr24UHQ)
++ [Harnessing the Power of Vim (workshop)](https://teamtreehouse.com/library/harnessing-the-power-of-vim)
++ [Your First Vim Plugin](https://youtu.be/lwD8G1P52Sk)
++ [Vim + Tmux](https://youtu.be/5r6yzFEXajQ)
