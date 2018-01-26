08118f9802fce4674755ce922bee5f1b6248c510
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-26 11:20:29 -0800

    rename notes folders to their topics

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+# Javascript
+
+## Node / V8 Engine 
+[https://blog.sessionstack.com/how-javascript-works-inside-the-v8-engine-5-tips-on-how-to-write-optimized-code-ac089e62b12e]
+
+## Web Workers
+[https://blog.sessionstack.com/how-javascript-works-the-building-blocks-of-web-workers-5-cases-when-you-should-use-them-a547c0757f6a]
+
+## Web Sockets
+[https://blog.sessionstack.com/how-javascript-works-deep-dive-into-websockets-and-http-2-with-sse-how-to-pick-the-right-path-584e6b8e3bf7]
+	
