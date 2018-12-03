387093cbced7a5315ff0821816a756d48b42b463
Billy Montgomery <billy.montgomery@gmail.com>
2018-12-03 14:48:40 -0800

    python/learning code

--- python/asteriks.py
+++
@@ -1,5 +0,0 @@
-fruits = ['lemon', 'pear', 'watermelon', 'tomato']
-print(fruits[0], fruits[1], fruits[2], fruits[3])
-print(*fruits)
-
-
--- python/leARNING/02_http-example.py
+++
@@ -1,4 +0,0 @@
-import urllib2
-response = urllib2.urlopen('http://python.org/')
-html = response.read()
-print html
