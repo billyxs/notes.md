9eb6917493f1913ba0b4acfc3f67ee8bc0aa5418
Billy Montgomery <billy.montgomery@gmail.com>
2018-12-06 15:52:46 -0800

    Saved at 2018-12-06 23:52:46 UTC

--- python/learning/tensorflow/test.py
+++ python/learning/tensorflow/test.py
@@ -1,6 +1,33 @@
+#https://www.sohamkamani.com/blog/2018/01/07/tensorflow-introduction/
+
 import tensorflow as tf
-from tensorflow.keras import layers
 
 
 print(tf.VERSION)
-print(tf.keras.__version__)
+
+a = tf.constant(3.0)
+b = tf.constant(2.0)
+
+c = a+b
+
+sess = tf.Session()
+
+print(sess.run(c))
+
+d = tf.placeholder(tf.float32)
+e = tf.placeholder(tf.float32)
+
+f = d+e
+
+sess2 = tf.Session()
+
+print(sess2.run(f, {d: 1, e: 5}))
+
+g = tf.placeholder(tf.float32, shape=(2,1))
+h = tf.placeholder(tf.float32, shape=(1,2))
+
+i = tf.matmul(g, h)
+
+sess3 = tf.Session()
+
+print(sess3.run(i, {g: [[1],[2]], h: [[3, 4]]}))
